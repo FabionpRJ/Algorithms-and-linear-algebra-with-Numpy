5a16b4cf057d32903cee24296654d7996be786ee
Fabionp_RJ <fabiopatao20@gmail.com>
2024-05-30 13:19:50 -0300

    Back substitution

--- Algorithms/linear_algebra_algorithms.py
+++ Algorithms/linear_algebra_algorithms.py
@@ -28,5 +28,22 @@ def permutate_matrix(matrix,row_1=0, row_2=0, Permutation=0):
     P[row_2] = swap
     return matrix, P                                    #Return the matrix, and P
 
+#---------------------------------------------- BACK SUBSTITUTION --------------------------------
+# Algorithm to find solutions in upper triangular matrices.
+# 
+# Receives a Upper Triangular matrix, and a solution matrix
+#
+# Output: Find the value of the unknows that leave the collumns in the solution matrix.
+#
+# Upper_matrix * y = Solution
+def back_substitution(upper_matrix, solution):
+    vector_y = np.zeros((upper_matrix.shape[1],1))                                                #
+    for i in range((upper_matrix.shape[0]-1),-1,-1):               #Execute over the rows
+        value = 0 
+        for j in range(i+1,upper_matrix.shape[1]):           #Execute over the collumns... on the right
+            if i != j:
+                value += upper_matrix[i][j]*vector_y[j][0]
+        vector_y[i][0] = (solution[i][0] - value)/upper_matrix[i][i]  #Define one solution element
+        value = 0                                               #resets the values...
+    return vector_y
     
-
